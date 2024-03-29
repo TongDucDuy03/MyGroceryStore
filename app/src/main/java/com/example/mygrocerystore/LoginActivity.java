f317336d1bc409f164ce7e98353213facee65f95
Tống Đức Duy <tongducduy4@gmail.com>
2024-03-29 19:38:11 +0700

    (Modern login and register )+welcome screen

--- app/src/main/java/com/example/mygrocerystore/LoginActivity.java
+++ app/src/main/java/com/example/mygrocerystore/LoginActivity.java
@@ -2,13 +2,34 @@ package com.example.mygrocerystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button signIn;
+    EditText email,password;
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        signUp = findViewById(R.id.sign_up);
+        signIn = findViewById(R.id.login_btn);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mygrocerystore/RegisterActivity.java
+++ app/src/main/java/com/example/mygrocerystore/RegisterActivity.java
@@ -2,13 +2,34 @@ package com.example.mygrocerystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    TextView signIn;
+    EditText name,email,password;
+    Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        name = findViewById(R.id.name_reg);
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        signUp = findViewById(R.id.reg_btn);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
     }
 }
