ed6305734d2acf68b0ab035598e38a2151f32647
Tống Đức Duy <tongducduy4@gmail.com>
2024-03-30 20:52:12 +0700

    Login and Registration Using Firebase Authentication+Progress Bar

--- app/src/main/java/com/example/mygrocerystore/LoginActivity.java
+++ app/src/main/java/com/example/mygrocerystore/LoginActivity.java
@@ -1,23 +1,42 @@
 package com.example.mygrocerystore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mygrocerystore.models.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button signIn;
     EditText email,password;
     TextView signUp;
+
+    FirebaseAuth auth;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        progressBar =findViewById(R.id.progreesbar);
+        progressBar.setVisibility(View.GONE);
 
         email = findViewById(R.id.email_login);
         password = findViewById(R.id.password_login);
@@ -31,5 +50,48 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginUser();
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+    }
+
+    private void loginUser() {
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this,"Email is Empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this,"Password is Empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(userPassword.length() < 6){
+            Toast.makeText(this,"Password length must be greater than 6 letter",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //login user
+        auth.signInWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(LoginActivity.this,"Login Successfull",Toast.LENGTH_SHORT).show();
+                        }else {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(LoginActivity.this,"Error"+task.isSuccessful(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/mygrocerystore/MainActivity.java
+++ app/src/main/java/com/example/mygrocerystore/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,17 +18,24 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mygrocerystore.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+    FirebaseAuth auth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progreesbar);
+        progressBar.setVisibility(View.GONE);
+        if(auth.getCurrentUser() != null){
+            Toast.makeText(this,"please wait you are already logged in",Toast.LENGTH_SHORT).show();
+        }
     }
 
 
