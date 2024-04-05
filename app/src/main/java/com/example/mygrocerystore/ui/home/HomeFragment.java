4602ab955d848b270cc8b749da23f511e1eeb62f
Tống Đức Duy <tongducduy4@gmail.com>
2024-04-06 01:17:08 +0700

    home_cat_item (Explore items)

--- app/src/main/java/com/example/mygrocerystore/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mygrocerystore/ui/home/HomeFragment.java
@@ -15,8 +15,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mygrocerystore.R;
+import com.example.mygrocerystore.adapters.HomeAdapter;
 import com.example.mygrocerystore.adapters.PopularAdapters;
 import com.example.mygrocerystore.databinding.FragmentHomeBinding;
+import com.example.mygrocerystore.models.HomeCategory;
 import com.example.mygrocerystore.models.PopularModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -31,15 +33,21 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
 
-    RecyclerView popularRec;
+    RecyclerView popularRec,homeCatRec;
     FirebaseFirestore db;
+    //popular item
     List<PopularModel> popularModellist;
     PopularAdapters popularAdapters;
+
+    //Home category
+    List<HomeCategory> categoryList;
+    HomeAdapter homeAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         db = FirebaseFirestore.getInstance();
         popularRec = root.findViewById(R.id.pop_rec);
+        homeCatRec = root.findViewById(R.id.explore_rec);
 
         //popular item
         popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
@@ -62,6 +70,29 @@ public class HomeFragment extends Fragment {
                         } else {
                             Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
                             Log.d("TAG", "Thông điệp của bạn ở đâys");
+                        }
+                    }
+                });
+
+        //Home Category
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryList = new ArrayList<>();
+        homeAdapter = new HomeAdapter(getActivity(),categoryList);
+        homeCatRec.setAdapter(homeAdapter);
+
+        db.collection("HomeCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                HomeCategory homeCategory = document.toObject(HomeCategory.class);
+                                categoryList.add(homeCategory);
+                                homeAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
