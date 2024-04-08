7807a1f42ee8dae6a35185047159a739c5271c92
Tống Đức Duy <tongducduy4@gmail.com>
2024-04-08 14:24:26 +0700

    Recommended+ fix ScrollView

--- app/src/main/java/com/example/mygrocerystore/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mygrocerystore/ui/home/HomeFragment.java
@@ -17,9 +17,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.mygrocerystore.R;
 import com.example.mygrocerystore.adapters.HomeAdapter;
 import com.example.mygrocerystore.adapters.PopularAdapters;
+import com.example.mygrocerystore.adapters.RecommendAdapter;
 import com.example.mygrocerystore.databinding.FragmentHomeBinding;
 import com.example.mygrocerystore.models.HomeCategory;
 import com.example.mygrocerystore.models.PopularModel;
+import com.example.mygrocerystore.models.RecommendedModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
@@ -33,7 +35,7 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
 
-    RecyclerView popularRec,homeCatRec;
+    RecyclerView popularRec,homeCatRec,recommendedRec;
     FirebaseFirestore db;
     //popular item
     List<PopularModel> popularModellist;
@@ -42,12 +44,17 @@ public class HomeFragment extends Fragment {
     //Home category
     List<HomeCategory> categoryList;
     HomeAdapter homeAdapter;
+
+    //Recommended
+    List<RecommendedModel> recommendedModelList;
+    RecommendAdapter recommendAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         db = FirebaseFirestore.getInstance();
         popularRec = root.findViewById(R.id.pop_rec);
         homeCatRec = root.findViewById(R.id.explore_rec);
+        recommendedRec = root.findViewById(R.id.recommend_rec);
 
         //popular item
         popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
@@ -65,11 +72,9 @@ public class HomeFragment extends Fragment {
                                 PopularModel popularModel = document.toObject(PopularModel.class);
                                 popularModellist.add(popularModel);
                                 popularAdapters.notifyDataSetChanged();
-                                Log.d("TAG", "Thông điệp của bạn ở đây");
                             }
                         } else {
                             Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
-                            Log.d("TAG", "Thông điệp của bạn ở đâys");
                         }
                     }
                 });
@@ -97,6 +102,29 @@ public class HomeFragment extends Fragment {
                     }
                 });
 
+
+        //Recommended
+        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        recommendedModelList = new ArrayList<>();
+        recommendAdapter = new RecommendAdapter(getActivity(),recommendedModelList);
+        recommendedRec.setAdapter(recommendAdapter);
+
+        db.collection("Recommended")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                RecommendedModel recommendedModel = document.toObject(RecommendedModel.class);
+                                recommendedModelList.add(recommendedModel);
+                                recommendAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
         return root;
     }
 
