d48569df30a2a0e2c857c884b3605214553b02c2
Tống Đức Duy <tongducduy4@gmail.com>
2024-04-10 16:34:35 +0700

    CardView

---
+++ app/src/main/java/com/example/mygrocerystore/models/NavCategroryModel.java
@@ -0,0 +1,43 @@
+package com.example.mygrocerystore.models;
+
+public class NavCategroryModel {
+    String description;
+    String name;
+    String discount;
+    String img_url;
+
+    public NavCategroryModel() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+}
--- app/src/main/java/com/example/mygrocerystore/ui/category/CategoryFragment.java
+++ app/src/main/java/com/example/mygrocerystore/ui/category/CategoryFragment.java
@@ -1,24 +1,67 @@
 package com.example.mygrocerystore.ui.category;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mygrocerystore.R;
+import com.example.mygrocerystore.adapters.HomeAdapter;
+import com.example.mygrocerystore.adapters.NavCategoryAdapter;
+import com.example.mygrocerystore.models.HomeCategory;
+import com.example.mygrocerystore.models.NavCategroryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
-
+    RecyclerView recyclerView;
+    FirebaseFirestore db;
+    List<NavCategroryModel> categroryModelList;
+    NavCategoryAdapter navCategoryAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_category, container, false);;
+        View root = inflater.inflate(R.layout.fragment_category, container, false);
+        db = FirebaseFirestore.getInstance();
+        recyclerView = root.findViewById(R.id.cat_rec);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        categroryModelList = new ArrayList<>();
+        navCategoryAdapter = new NavCategoryAdapter(getActivity(), categroryModelList);
+        recyclerView.setAdapter(navCategoryAdapter);
+
+        db.collection("NavCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                NavCategroryModel navCategroryModel = document.toObject(NavCategroryModel.class);
+                                categroryModelList.add(navCategroryModel);
+                                navCategoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
         return root;
     }
