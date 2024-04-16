a35172ce4b274537dfc8b03b43e022bd67eb4665
Tống Đức Duy <tongducduy4@gmail.com>
2024-04-16 13:45:09 +0700

    Detailed Products

--- app/src/main/java/com/example/mygrocerystore/adapters/ViewAllAdapter.java
+++ app/src/main/java/com/example/mygrocerystore/adapters/ViewAllAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mygrocerystore.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.mygrocerystore.R;
+import com.example.mygrocerystore.activities.DetailedActivity;
 import com.example.mygrocerystore.models.ViewAllModel;
 
 import org.w3c.dom.Text;
@@ -49,6 +51,15 @@ public class ViewAllAdapter extends RecyclerView.Adapter<ViewAllAdapter.ViewHold
         if(list.get(position).getType().equals("milk")){
             holder.price.setText(list.get(position).getPrice()+"/litre");
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailedActivity.class);
+                intent.putExtra("detail",list.get(position));
+                context.startActivity(intent);
+            }
+        });
 
     }
 
